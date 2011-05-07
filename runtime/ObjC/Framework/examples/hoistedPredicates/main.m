6be49d39cb84acda8ae719062386afe1555473d2
acondit <acondit@parrt.spork>
2011-05-07 08:09:08 -0800

    Changes to match ANTLR3.4 revision.
    [git-p4: depot-paths = "//depot/code/antlr/antlr3-main/": change = 8415]

--- runtime/ObjC/Framework/examples/hoistedPredicates/main.m
+++ runtime/ObjC/Framework/examples/hoistedPredicates/main.m
@@ -1,5 +1,5 @@
 #import <Cocoa/Cocoa.h>
-#import <antlr3.h>
+#import <ANTLR/ANTLR.h>
 #import "TLexer.h"
 #import "TParser.h"
 
@@ -7,7 +7,7 @@ int main() {
     NSError *error;
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
-	NSString *string = [NSString stringWithContentsOfFile:@"../../examples/hoistedPredicates/input" encoding:NSASCIIStringEncoding error:&error];
+	NSString *string = [NSString stringWithContentsOfFile:@"/Users/acondit/source/antlr3/acondit_localhost/code/antlr/antlr3-main/runtime/ObjC/Framework/examples/hoistedPredicates/input" encoding:NSASCIIStringEncoding error:&error];
 	NSLog(@"input is : %@", string);
 	ANTLRStringStream *stream = [ANTLRStringStream newANTLRStringStream:string];
 	TLexer *lexer = [TLexer newTLexerWithCharStream:stream];
