5ebc0f6132b4e6128bd88fb518c5a04c26ba15dc
acondit <acondit@parrt.spork>
2011-05-07 08:07:52 -0800

    Changes to Match ANTLR3.4 revision.
    [git-p4: depot-paths = "//depot/code/antlr/antlr3-main/": change = 8413]

--- runtime/ObjC/Framework/examples/hoistedPredicates/TLexer.h
+++ runtime/ObjC/Framework/examples/hoistedPredicates/TLexer.h
@@ -1,10 +1,10 @@
-// $ANTLR 3.2 Aug 24, 2010 10:45:57 T.g 2010-08-24 13:53:45
+// $ANTLR ${project.version} ${buildNumber} T.g 2011-05-06 19:14:23
 
 /* =============================================================================
  * Standard antlr3 OBJC runtime definitions
  */
 #import <Cocoa/Cocoa.h>
-#import "antlr3.h"
+#import <ANTLR/ANTLR.h>
 /* End of standard antlr3 runtime definitions
  * =============================================================================
  */
@@ -14,21 +14,28 @@
 #pragma mark Rule return scopes start
 #pragma mark Rule return scopes end
 #pragma mark Tokens
-#define WS 6
-#define INT 5
-#define ID 4
+#ifdef EOF
+#undef EOF
+#endif
 #define EOF -1
 #define T__7 7
+#define ID 4
+#define INT 5
+#define WS 6
+/* interface lexer class */
 @interface TLexer : ANTLRLexer { // line 283
-// start of actions.lexer.memVars
-// start of action-actionScope-memVars
+/* ObjC start of actions.lexer.memVars */
+/* ObjC end of actions.lexer.memVars */
 }
++ (void) initialize;
 + (TLexer *)newTLexerWithCharStream:(id<ANTLRCharStream>)anInput;
-
-- (void)mT__7; 
-- (void)mID; 
-- (void)mINT; 
-- (void)mWS; 
-- (void)mTokens; 
+/* ObjC start actions.lexer.methodsDecl */
+/* ObjC end actions.lexer.methodsDecl */
+- (void) mT__7 ; 
+- (void) mID ; 
+- (void) mINT ; 
+- (void) mWS ; 
+- (void) mTokens ; 
 
 @end /* end of TLexer interface */
+
--- runtime/ObjC/Framework/examples/hoistedPredicates/TParser.h
+++ runtime/ObjC/Framework/examples/hoistedPredicates/TParser.h
@@ -1,43 +1,73 @@
-// $ANTLR 3.2 Aug 24, 2010 10:45:57 T.g 2010-08-24 13:53:44
+// $ANTLR ${project.version} ${buildNumber} T.g 2011-05-06 19:14:23
 
 /* =============================================================================
  * Standard antlr3 OBJC runtime definitions
  */
 #import <Cocoa/Cocoa.h>
-#import "antlr3.h"
+#import <ANTLR/ANTLR.h>
 /* End of standard antlr3 runtime definitions
  * =============================================================================
  */
 
 /* parserHeaderFile */
+#ifndef ANTLR3TokenTypeAlreadyDefined
+#define ANTLR3TokenTypeAlreadyDefined
+typedef enum {
+    ANTLR_EOF = -1,
+    INVALID,
+    EOR,
+    DOWN,
+    UP,
+    MIN
+} ANTLR3TokenType;
+#endif
+
 #pragma mark Tokens
-#define WS 6
-#define INT 5
-#define ID 4
+#ifdef EOF
+#undef EOF
+#endif
 #define EOF -1
 #define T__7 7
+#define ID 4
+#define INT 5
+#define WS 6
 #pragma mark Dynamic Global Scopes
 #pragma mark Dynamic Rule Scopes
 #pragma mark Rule Return Scopes start
-#pragma mark Rule return scopes end
+
+/* Interface grammar class */
 @interface TParser : ANTLRParser { /* line 572 */
-// start of globalAttributeScopeMemVar
+/* ObjC start of ruleAttributeScopeMemVar */
 
 
-// start of action-actionScope-memVars
+/* ObjC end of ruleAttributeScopeMemVar */
+/* ObjC start of globalAttributeScopeMemVar */
+
+
+/* ObjC end of globalAttributeScopeMemVar */
+/* ObjC start of actions.(actionScope).memVars */
 
 /* With this true, enum is seen as a keyword.  False, it's an identifier */
 BOOL enableEnum;
 
-// start of ruleAttributeScopeMemVar
-
-
-// Start of memVars
+/* ObjC end of actions.(actionScope).memVars */
+/* ObjC start of memVars */
+/* ObjC end of memVars */
 
  }
 
-// start of action-actionScope-methodsDecl
+/* ObjC start of actions.(actionScope).properties */
+/* ObjC end of actions.(actionScope).properties */
+/* ObjC start of properties */
+/* ObjC end of properties */
 
++ (void) initialize;
++ (id) newTParser:(id<ANTLRTokenStream>)aStream;
+/* ObjC start of actions.(actionScope).methodsDecl */
+/* ObjC end of actions.(actionScope).methodsDecl */
+
+/* ObjC start of methodsDecl */
+/* ObjC end of methodsDecl */
 
 - (void)stat; 
 - (void)identifier; 
@@ -46,6 +76,7 @@ BOOL enableEnum;
 
 
 @end /* end of TParser interface */
+
 /** Demonstrates how semantic predicates get hoisted out of the rule in 
  *  which they are found and used in other decisions.  This grammar illustrates
  *  how predicates can be used to distinguish between enum as a keyword and
