f3fb1c379bd8ac2ad999499c0d6739657b1d9886
acondit <acondit@parrt.spork>
2011-05-07 08:18:20 -0800

    Changes to match ANTLR3.4 revision.
    [git-p4: depot-paths = "//depot/code/antlr/antlr3-main/": change = 8421]

--- runtime/ObjC/Framework/examples/treerewrite/TreeRewriteLexer.h
+++ runtime/ObjC/Framework/examples/treerewrite/TreeRewriteLexer.h
@@ -1,10 +1,10 @@
-// $ANTLR 3.2 Aug 24, 2010 10:45:57 TreeRewrite.g 2010-08-24 14:18:09
+// $ANTLR ${project.version} ${buildNumber} TreeRewrite.g 2011-05-06 18:56:28
 
 /* =============================================================================
  * Standard antlr3 OBJC runtime definitions
  */
 #import <Cocoa/Cocoa.h>
-#import "antlr3.h"
+#import <ANTLR/ANTLR.h>
 /* End of standard antlr3 runtime definitions
  * =============================================================================
  */
@@ -14,17 +14,24 @@
 #pragma mark Rule return scopes start
 #pragma mark Rule return scopes end
 #pragma mark Tokens
+#ifdef EOF
+#undef EOF
+#endif
+#define EOF -1
 #define INT 4
 #define WS 5
-#define EOF -1
+/* interface lexer class */
 @interface TreeRewriteLexer : ANTLRLexer { // line 283
-// start of actions.lexer.memVars
-// start of action-actionScope-memVars
+/* ObjC start of actions.lexer.memVars */
+/* ObjC end of actions.lexer.memVars */
 }
++ (void) initialize;
 + (TreeRewriteLexer *)newTreeRewriteLexerWithCharStream:(id<ANTLRCharStream>)anInput;
-
-- (void)mINT; 
-- (void)mWS; 
-- (void)mTokens; 
+/* ObjC start actions.lexer.methodsDecl */
+/* ObjC end actions.lexer.methodsDecl */
+- (void) mINT ; 
+- (void) mWS ; 
+- (void) mTokens ; 
 
 @end /* end of TreeRewriteLexer interface */
+
--- runtime/ObjC/Framework/examples/treerewrite/TreeRewriteParser.h
+++ runtime/ObjC/Framework/examples/treerewrite/TreeRewriteParser.h
@@ -1,76 +1,124 @@
-// $ANTLR 3.2 Aug 24, 2010 10:45:57 TreeRewrite.g 2010-08-24 14:18:09
+// $ANTLR ${project.version} ${buildNumber} TreeRewrite.g 2011-05-06 18:56:28
 
 /* =============================================================================
  * Standard antlr3 OBJC runtime definitions
  */
 #import <Cocoa/Cocoa.h>
-#import "antlr3.h"
+#import <ANTLR/ANTLR.h>
 /* End of standard antlr3 runtime definitions
  * =============================================================================
  */
 
 /* parserHeaderFile */
+#ifndef ANTLR3TokenTypeAlreadyDefined
+#define ANTLR3TokenTypeAlreadyDefined
+typedef enum {
+    ANTLR_EOF = -1,
+    INVALID,
+    EOR,
+    DOWN,
+    UP,
+    MIN
+} ANTLR3TokenType;
+#endif
+
 #pragma mark Tokens
-#define WS 5
-#define INT 4
+#ifdef EOF
+#undef EOF
+#endif
 #define EOF -1
+#define INT 4
+#define WS 5
 #pragma mark Dynamic Global Scopes
 #pragma mark Dynamic Rule Scopes
 #pragma mark Rule Return Scopes start
-/* returnScopeInterface */
-@interface TreeRewriteParser_rule_return :ANTLRParserRuleReturnScope { /* returnScopeInterface line 1806 */
+/* returnScopeInterface TreeRewriteParser_rule_return */
+@interface TreeRewriteParser_rule_return :ANTLRParserRuleReturnScope { /* returnScopeInterface line 1838 */
 /* AST returnScopeInterface.memVars */
-ANTLRCommonTree *tree; /* start of memVars() */
+ANTLRCommonTree *tree; /* ObjC start of memVars() */
 }
+/* start properties */
 /* AST returnScopeInterface.properties */
-@property (retain, getter=getTree, setter=setTree:) ANTLRCommonTree *tree; /* start properties */
+@property (retain, getter=getTree, setter=setTree:) ANTLRCommonTree *tree;
 + (TreeRewriteParser_rule_return *)newTreeRewriteParser_rule_return;
 /* this is start of set and get methods */
-/* AST returnScopeInterface.methodsdecl */
+/* AST returnScopeInterface.methodsDecl */
 - (ANTLRCommonTree *)getTree;
+
 - (void) setTree:(ANTLRCommonTree *)aTree;
   /* methodsDecl */
 @end /* end of returnScopeInterface interface */
-/* returnScopeInterface */
-@interface TreeRewriteParser_subrule_return :ANTLRParserRuleReturnScope { /* returnScopeInterface line 1806 */
+
+
+
+/* returnScopeInterface TreeRewriteParser_subrule_return */
+@interface TreeRewriteParser_subrule_return :ANTLRParserRuleReturnScope { /* returnScopeInterface line 1838 */
 /* AST returnScopeInterface.memVars */
-ANTLRCommonTree *tree; /* start of memVars() */
+ANTLRCommonTree *tree; /* ObjC start of memVars() */
 }
+/* start properties */
 /* AST returnScopeInterface.properties */
-@property (retain, getter=getTree, setter=setTree:) ANTLRCommonTree *tree; /* start properties */
+@property (retain, getter=getTree, setter=setTree:) ANTLRCommonTree *tree;
 + (TreeRewriteParser_subrule_return *)newTreeRewriteParser_subrule_return;
 /* this is start of set and get methods */
-/* AST returnScopeInterface.methodsdecl */
+/* AST returnScopeInterface.methodsDecl */
 - (ANTLRCommonTree *)getTree;
+
 - (void) setTree:(ANTLRCommonTree *)aTree;
   /* methodsDecl */
 @end /* end of returnScopeInterface interface */
 
-#pragma mark Rule return scopes end
+
+
+
+/* Interface grammar class */
 @interface TreeRewriteParser : ANTLRParser { /* line 572 */
-// start of globalAttributeScopeMemVar
+/* ObjC start of ruleAttributeScopeMemVar */
 
 
-// start of action-actionScope-memVars
-// start of ruleAttributeScopeMemVar
+/* ObjC end of ruleAttributeScopeMemVar */
+/* ObjC start of globalAttributeScopeMemVar */
 
 
-// Start of memVars
+/* ObjC end of globalAttributeScopeMemVar */
+/* ObjC start of actions.(actionScope).memVars */
+/* ObjC end of actions.(actionScope).memVars */
+/* ObjC start of memVars */
 /* AST parserHeaderFile.memVars */
-/* AST parsermemVars */
-id<ANTLRTreeAdaptor> treeAdaptor;
+NSInteger ruleLevel;
+NSArray *ruleNames;
+  /* AST super.memVars */
+/* AST parserMemVars */
+id<ANTLRTreeAdaptor> treeAdaptor;   /* AST parserMemVars */
+/* ObjC end of memVars */
 
  }
 
-// start of action-actionScope-methodsDecl
+/* ObjC start of actions.(actionScope).properties */
+/* ObjC end of actions.(actionScope).properties */
+/* ObjC start of properties */
+/* AST parserHeaderFile.properties */
+  /* AST super.properties */
+/* AST parserProperties */
+@property (retain, getter=getTreeAdaptor, setter=setTreeAdaptor:) id<ANTLRTreeAdaptor> treeAdaptor;   /* AST parserproperties */
+/* ObjC end of properties */
 
-/* AST parserHeaderFile.methodsdecl */
++ (void) initialize;
++ (id) newTreeRewriteParser:(id<ANTLRTokenStream>)aStream;
+/* ObjC start of actions.(actionScope).methodsDecl */
+/* ObjC end of actions.(actionScope).methodsDecl */
+
+/* ObjC start of methodsDecl */
+/* AST parserHeaderFile.methodsDecl */
+  /* AST super.methodsDecl */
 /* AST parserMethodsDecl */
 - (id<ANTLRTreeAdaptor>) getTreeAdaptor;
-- (void) setTreeAdaptor:(id<ANTLRTreeAdaptor>)theTreeAdaptor;
+- (void) setTreeAdaptor:(id<ANTLRTreeAdaptor>)theTreeAdaptor;   /* AST parsermethodsDecl */
+/* ObjC end of methodsDecl */
 
 - (TreeRewriteParser_rule_return *)rule; 
 - (TreeRewriteParser_subrule_return *)subrule; 
 
 
 @end /* end of TreeRewriteParser interface */
+
